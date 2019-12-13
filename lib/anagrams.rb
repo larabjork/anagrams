fd9bc923209db78cbd9f78ab2300e7107f22e0c6
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-13 13:05:28 -0800

    add passing code for that identifies an anagram

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,14 +5,17 @@ class Anagram
   end
 
   def evaluator(word1, word2)
-    tester1 = @word1.split(//).sort.join.to_s
-    tester2 = @word2.split(//).sort.join.to_s
-    if (tester1[/[(aeiou)]/] && tester1.length > 1) && (tester2[/[(aeiou)]/] && tester2.length > 1) && (tester1.length == tester2.length)
-      puts "You've got words that we can compare!"
-      return "yes word"
-    elsif (tester1.length == 1) || (tester2.length == 1)
+    tester1 = word1.split(//).sort.join.to_s
+    tester2 = word2.split(//).sort.join.to_s
+    if (tester1.length == 1) || (tester2.length == 1)
       puts "Your word is too short"
       "too short"
+    elsif (tester1 == tester2)
+      puts "Woohoo! You've got anagrams!"
+      "yes anagram"
+    elsif (tester1[/[(aeiou)]/] && tester1.length > 1) && (tester2[/[(aeiou)]/] && tester2.length > 1) && (tester1.length == tester2.length)
+      puts "You've got words that we can compare!"
+      return "yes word"
     elsif tester1.length != tester2.length
       puts "Your words are different lengths, so they can't be anagrams of each other"
       "not anagram"
