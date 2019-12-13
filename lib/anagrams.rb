063e9dabcb0251ff6241359effbcf5d5e9a4fe67
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-13 12:53:24 -0800

    add failing test to check for anagrams

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,16 +5,18 @@ class Anagram
   end
 
   def evaluator(word1, word2)
-    if (@word1[/[(aeiou)]/] && @word1.length > 1) && (@word2[/[(aeiou)]/] && @word2.length > 1) && (@word1.length == @word2.length)
+    tester1 = @word1.split(//).sort.join.to_s
+    tester2 = @word2.split(//).sort.join.to_s
+    if (tester1[/[(aeiou)]/] && tester1.length > 1) && (tester2[/[(aeiou)]/] && tester2.length > 1) && (tester1.length == tester2.length)
       puts "You've got words that we can compare!"
       return "yes word"
-    elsif (@word1.length == 1) || (@word2.length == 1)
+    elsif (tester1.length == 1) || (tester2.length == 1)
       puts "Your word is too short"
       "too short"
-    elsif @word1.length != @word2.length
+    elsif tester1.length != tester2.length
       puts "Your words are different lengths, so they can't be anagrams of each other"
       "not anagram"
-    elsif (@word1[/[(^aeiou)]/]) || (@word2[/[(^aeiou)]/])
+    elsif (tester1[/[(^aeiou)]/]) || (tester2[/[(^aeiou)]/])
       puts "Hang on, I think you need to buy a vowel or two."
       "no vowel"
     end
