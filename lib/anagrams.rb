c0ca759c31748d148e77a7c2788df3dd49e4af0c
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-13 14:24:56 -0800

    add passing code that disregards capitalization

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,20 +5,23 @@ class Anagram
   end
 
   def evaluator(word1, word2)
-    tester1 = word1.split(//).sort.join.to_s
-    tester2 = word2.split(//).sort.join.to_s
+    lowercase1 = word1.downcase
+    lowercase2 = word2.downcase
+
+    tester1 = lowercase1.split(//).sort.join.to_s
+    tester2 = lowercase2.split(//).sort.join.to_s
     if (tester1.length == 1) || (tester2.length == 1)
       puts "Your word is too short"
       "too short"
     elsif (tester1[/^[^aeyiuo]+$/]) || (tester2[/^[^aeyiuo]+$/])
       puts "Hang on, I think you need to buy a vowel or two."
-      "no vowel" 
-    elsif tester1 != tester2
-      puts "You don't have an anagram"
-      "not anagram"
+      "no vowel"
     elsif (tester1[/[aeiou]/]) && (tester2[/[aeiou]/]) && (tester1 == tester2)
       puts "Woohoo! You've got anagrams!"
       "yes anagram"
+    elsif tester1 != tester2
+      puts "You don't have an anagram"
+      "not anagram"
 
     end
   end
